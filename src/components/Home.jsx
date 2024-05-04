73ed0f8c12d913bc034cba727156599d6de7ef09
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2024-05-04 16:20:39 -0700

    •Increased the font size of the header and footer •Footer is sticky •Header follows •Added content to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,41 @@
-// src/components/AboutPage.jsx
-import React from 'react';
+import React, { useRef } from 'react';
+import style from './home.module.css';
 
 function HomePage() {
+  const audioRef = useRef(null);  // Create a ref for the audio element
+
+  const handleMouseEnter = () => {
+    if (audioRef.current) {
+      audioRef.current.play();  // Play audio on mouse enter
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();  // Pause audio on mouse leave
+      audioRef.current.currentTime = 0;  // Reset audio to start
+    }
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>This is the about page where we discuss our mission and vision.</p>
+    <div className={style.container}>
+      <div className={style.leftColumn}>
+        <h1 className={style.intro_name}>I am <strong>Nicholas Chung</strong>, a</h1>
+        <h2 className={style.headline}>Full Stack Software Engineer</h2>
+        <h3 className={style.statement}>"Together as one."</h3>
+      </div>
+      <div className={style.rightColumn}>
+        <img 
+          src="public/casual_picture_2.jpg" 
+          alt="Nicholas Chung" 
+          className={style.personal_image}
+          onMouseEnter={handleMouseEnter} 
+          onMouseLeave={handleMouseLeave}
+        />
+        <audio ref={audioRef} src="public/Among Us Sound Effect.mp3" preload="auto">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
     </div>
   );
 }
