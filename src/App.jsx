320dc9c9c83f701ba4428b0fe37d79ad06dd9ea2
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2024-05-03 18:12:18 -0700

    Added icons to my navbar buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Projects from './components/Projects';
 import Home from './components/Home';
 
 const pages = [
-  { name: 'about', title: 'About' },
-  { name: 'projects', title: 'Projects' },
-  { name: 'contact', title: 'Contact' },
+  { name: 'about', title: 'About', icon: 'fa fa-id-card-o'},
+  { name: 'projects', title: 'Projects', icon: 'fa fa-briefcase'},
+  { name: 'contact', title: 'Contact', icon:'fa fa-commenting'},
 ];
 
 function App() {
@@ -28,16 +28,16 @@ function App() {
       <header>
         <nav>
           <div className="nav-left">
-            <button onClick={() => setCurrentPage('home')}>Home</button>
+            <button onClick={() => setCurrentPage('home')}><i class="fa fa-home" aria-hidden="true"></i>Home</button>
           </div>
           <div className="nav-right">
             {pages.map(page => (
               <button key={page.name} onClick={() => setCurrentPage(page.name)}>
-                {page.title}
+                <i className={page.icon}></i> {page.title}
               </button>
             ))}
             <a href="public/Nick's Resume (Updated 10.14.2023).pdf" target="_blank" rel="noopener noreferrer">
-              <button>Resume</button>
+              <button><i className="fa fa-file-pdf-o"></i> Resume</button>
             </a>
           </div>
         </nav>
