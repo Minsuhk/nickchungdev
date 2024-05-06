6f31377df2948efef190dd1a7bc65ee0827e8851
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2024-05-05 21:02:06 -0700

    •Adjusted footer text and icons for responsiveness •Added email button to footer •Setup the Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,22 @@
-// src/components/AboutPage.jsx
-import React from 'react';
+import React, { useRef } from 'react';
+import style from './contact.module.css';
 
 function ContactPage() {
   return (
     <div>
-      <h1>Contact</h1>
-      <p>CONTACT</p>
+      <h1 className={style.call_to_action}>C'MON, SAY HI!!</h1>
+      <div className={style.call_out_text}>
+        You came to this page to see how to contact me, right?
+        That means you are interested in contacting me, so why not contact me? 
+        Feel free to contact me.
+      </div>
+      <br />
+      <div className={style.socials}>
+        <a href="https://github.com/Minsuhk" target="_blank"><i class="fa fa-github"></i></a>
+        <a href="public/Nick's Resume (Updated 10.14.2023).pdf" target="_blank"><i class="fa fa-file-pdf-o"></i></a>
+        <a href="https://www.linkedin.com/in/nicholasmchung/" target="_blank"><i class="fa fa-linkedin"></i></a>
+        <a href="mailto: nicholasmchung@gmail.com" target="_blank"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+      </div>
     </div>
   );
 }
