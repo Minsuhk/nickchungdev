0bce7a7989f6b77d1d8f2673025777e77831ca94
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2024-05-08 02:07:19 -0700

    I want this to work

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // Point to the root directory
-  baseUrl: './', // Set base URL to the root
-  base: '/', // Adjust according to your specific environment if needed
 })
