522e2abea0200f57c84ff988c7097e39fa36be18
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2024-05-08 01:15:59 -0700

    try to fix vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // Point to the root directory
+  baseUrl: './', // Set base URL to the root
 })
