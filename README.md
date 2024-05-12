2bff30f9d1cdc41716119874ac384bb2d8c9c5d8
Nicholas Chung <xx.minsuhk.xx@gmail.com>
2024-05-12 01:27:23 -0700

    •Added features to the readme •Updated the technologies of the game in projects to include vscode •Updated the description for poekmon website and fruit catching game •Added exclamation points to the end of every description in projects

--- README.md
+++ README.md
@@ -12,3 +12,36 @@
     -In the terminal, a local link should have been created for you to click on. CTRL+Click on it to open the program on a browser
     -Now that the website is open, you can click on any buttons to explore the website
     -Easter Egg: Try hovering your cursor over my home screen picture to see what happens 
+
+■ ■ ■ Features ■ ■ ■
+
+•Home page 
+    -Nav bar with "Home", "About", "Projects", "Contact", "Resume" buttons
+    -Footer with credits on it and "GitHub", "Resume", "LinkedIn", and "Email" buttons
+    -Brief greeting text 
+    -Image of me
+    -Easter egg
+
+•About Page
+    -Nav bar with "Home", "About", "Projects", "Contact", "Resume" buttons
+    -Footer with credits on it and "GitHub", "Resume", "LinkedIn", and "Email" buttons
+    -Brief introduction of myself
+    -Another image of me 
+    -List of skills
+    -Carousel of experiences
+    -Images of hobbies I enjoy (left to right: robots, gaming, bowling)
+
+•Projects Page
+    -Nav bar with "Home", "About", "Projects", "Contact", "Resume" buttons
+    -Footer with credits on it and "GitHub", "Resume", "LinkedIn", and "Email" buttons
+    -Display of projects that I have worked on
+    -Each project has a short, creative description of what it does
+    -Each project has a list of technologies used to make it
+    -Each project has an image that is related to the project
+    -Each project has a link that takes the user to the github page associated with the project
+
+•Contact Page
+    -Nav bar with "Home", "About", "Projects", "Contact", "Resume" buttons
+    -Footer with credits on it and "GitHub", "Resume", "LinkedIn", and "Email" buttons
+    -Call-to-action text that motivates the user to contact me
+    -Four buttons that the user can use to contact me: "GitHub", "Resume", "LinkedIn", and "Email" button
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,18 +13,18 @@ function ProjectsPage() {
       <h1 className={style.call_to_action}>PROJECTS! 💻</h1>
       <div className={style.grid_container}>
           <div className={style.grid_item1}>
-              <div className={style.project_title}>PokeCheck Website</div>
-              <img src={pokecheck} alt="PokeCheck Website" style={{ width: '30%', height: 'auto' }} />
-              <p><strong>Technologies: HTML, CSS, JS, PokeAPI, VSCode</strong></p>
-              Everyone loves Pokemon, so check on the ones from your time
-              <br />
-              <a href="https://github.com/Minsuhk/PokedexWebsite" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
+            <div className={style.project_title}>PokeCheck Website</div>
+            <img src={pokecheck} alt="PokeCheck Website" style={{ width: '30%', height: 'auto' }} />
+            <p><strong>Technologies: HTML, CSS, JS, PokeAPI, VSCode</strong></p>
+            Gotta check them all, gotta check them all! Check on your fav pokemon on any browser!
+            <br />
+            <a href="https://github.com/Minsuhk/PokedexWebsite" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
           </div>
           <div className={style.grid_item2}>
             <div className={style.project_title}>CSUF Food Review App</div>
             <img src={foodreview} alt="CSUF Food Review App" style={{ width: '30%', height: 'auto' }} />
             <p><strong>Technologies: Swift, Firebase, XCodee</strong></p>
-            Feeling hungry at CSUF? Use this app
+            Feeling hungry at CSUF? Use this app!
             <br />
             <a href="https://github.com/CSUF-CPSC223W-2022S/project-group-9" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
           </div>
@@ -32,15 +32,15 @@ function ProjectsPage() {
             <div className={style.project_title}>React Personal Website</div>
             <img src={personal} alt="React Personal Website" style={{ width: '30%', height: 'auto' }} />
             <p><strong>Technologies: HTML, CSS, React, VSCode</strong></p>
-            A website all about me
+            A website all about me!
             <br />
             <a href="https://github.com/Minsuhk/349-Project-2" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
           </div>
           <div className={style.grid_item4}>
             <div className={style.project_title}>Fruit Catching Game</div>
             <img src={fruit} alt="Super duper fun happy honey bee farm harvest simulator 5000.2" style={{ width: '20%', height: 'auto' }} />
-            <p><strong>Technologies: C#, Unity</strong></p>
-            Games with cute drawings and fruit are fun games
+            <p><strong>Technologies: C#, Unity, VSCode</strong></p>
+            Gotta save the cute, falling fruits to save the farm!
             <br />
             <a href="https://github.com/Minsuhk/Happy-Farm-Game" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
           </div>
@@ -48,7 +48,7 @@ function ProjectsPage() {
             <div className={style.project_title}>T-Rex AI Model</div>
             <img src={ai} alt="T-Rex AI Model" style={{ width: '30%', height: 'auto' }} />
             <p><strong>Technologies: Python, HTML, CSS, VSCode</strong></p>
-            If we can't get over a score of 1000 on the Google T-Rex game, then we will train an AI to do it for us
+            If we can't get over a score of 1000 on the Google T-Rex game, then we will train an AI to do it for us!
             <br />
             <a href="https://github.com/JustinLieng/CPSC-481-Dino-AI" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
           </div>
@@ -56,7 +56,7 @@ function ProjectsPage() {
             <div className={style.project_title}>Moyai Game Info Website</div>
             <img src={moyai} alt="Moyai Game Info Website" style={{ width: '30%', height: 'auto' }} />
             <p><strong>Technologies: HTML, CSS, JS, Flask, MongoDB, YouTube API, SteamAPI, VSCode</strong></p>
-            The one-stop site for all your game info needs
+            The one-stop site for all your game info needs!
             <br />
             <a href="https://github.com/HunterBendel/Moyai" className = {style.learn_button} target="_blank"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i>Learn More!</a>
           </div>
